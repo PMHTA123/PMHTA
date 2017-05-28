2013301d049b3208dfcd27c847d9c47a8a792bf7
DiepPhung109 <huynhdiepphung109@gmail.com>
2017-05-29 01:16:45 +0700

    commit lan 1

---
+++ CSDL.sql
@@ -0,0 +1,12 @@
+﻿use CSDL
+GO
+------- Bảng đăng nhập-----
+create table DangNhap
+	(
+	idDangNhap varchar(20) primary key,
+	matKhau nvarchar (20),
+	trangThai bit
+	)
+insert into DangNhap values ('admin','admin',1)
+Go
+
